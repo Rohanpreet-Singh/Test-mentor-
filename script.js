349b82bbd1c9c13f1e28caca852f9062971b646e
Rohanpreet-Singh <advertisersagent@gmail.com>
2025-05-04 09:59:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateTest() {
+  alert("AI test generator would process your file here. (Placeholder)");
+}
+
+function downloadTest() {
+  alert("Download started (simulated). In real version, the test will be downloaded.");
+}
